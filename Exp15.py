92c483448b544ef1d39f1ca8009eb1aac5b42581
MEENA1311 <meena197110@gmail.com>
2025-05-02 20:00:32 +0530

    Create Exp15.py

---
+++ Exp15.py
@@ -0,0 +1,12 @@
+def is_happy(n):
+    seen = set()
+    while n != 1 and n not in seen:
+        seen.add(n)
+        n = sum(int(digit) ** 2 for digit in str(n))
+    return n == 1
+
+n = int(input("Enter a number: "))
+if is_happy(n):
+    print("Happy number")
+else:
+    print("Not a Happy number")
