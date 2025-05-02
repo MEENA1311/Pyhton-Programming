d0d3339877d9567b7aba1e9e67bd6dc27cd44124
MEENA1311 <meena197110@gmail.com>
2025-05-02 20:00:10 +0530

    Create Exp14.py

---
+++ Exp14.py
@@ -0,0 +1,12 @@
+def perfect(n):
+    sum = 0
+    for i in range(1, n // 2 + 1):
+        if n % i == 0:
+            sum += i
+    if sum == n:
+        print("Perfect number")
+    else:
+        print("Not a perfect number")
+
+n = int(input("Enter a number: "))
+perfect(n)
